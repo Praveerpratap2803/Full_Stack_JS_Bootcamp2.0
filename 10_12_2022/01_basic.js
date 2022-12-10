e0115c0d3c83cc538551fe00f35fb2de939b14a8
Praveerpratap2803 <praveerpratap2803@gmail.com>
2022-12-10 21:53:45 +0530

    10_12_2022

---
+++ 10_12_2022/01_basic.js
@@ -0,0 +1,11 @@
+let a=3;
+let b=4;
+let x=true;
+let y=null;
+console.log(typeof(x));
+console.log(typeof(y));
+let c=[1,"praveer",true];
+
+console.log("two number is",a,b);
+console.log("two number is" +a+b);
+
