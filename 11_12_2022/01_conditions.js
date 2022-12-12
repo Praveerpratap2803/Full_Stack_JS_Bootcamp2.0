576868135f461ee86843a30e4f08b55460c0c53b
Praveerpratap2803 <praveerpratap2803@gmail.com>
2022-12-12 22:16:39 +0530

    till 11_12_2022

--- 11_12_2022/01_conditions.js
+++ 11_12_2022/01_conditions.js
@@ -24,11 +24,70 @@ let rating = 5;
 // }
 
 
+// let raining = false;
+
+// raining ? console.log("raining"):console.log("not raining")
+
+// let array=[
+//     "a","b","c","d"
+// ]
+// console.log(array);
+// console.log(array[3]);
+// console.log(array[6]);
+
+// let obj = {
+//     fname:"Praveer",
+//     lname:"pratap",
+//     mobileno:90000,
+// }
+
+// console.log(obj.fname); 
 
 
+// Date
+// const now = new Date();
+
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getDate());
+// console.log(now.getMonth());
+// console.log(now.getHours());
+// console.log(now.getTime());
+
+//Math
+
+// const pi = Math.PI;
+// console.log(pi);
+
+// console.log(Math.round(9.4));
+// console.log(Math.floor(9.5))
+// console.log(Math.ceil(9.5))
+// console.log(Math.min(1,2,3,4))
+// console.log(Math.max(1,2,3,4))
+
+// console.log(Math.random());
+// console.log(Math.round(Math.random()*10));
+// console.log(Math.pow(3,2));
 
 
+// Loop
+let i=2;
+do{
 
+    console.log(i);
+    i--;
+}while(i>1);
+
+let j=3;
+while(j>1){
+    console.log(j);
+    j--;
+}
+
+for(let i=0;i<5;i++){
+    console.log(i);
+}
 
 
 
