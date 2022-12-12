01f7ef58644b2534872e249caa8847ba5847ce7f
Praveerpratap2803 <praveerpratap2803@gmail.com>
2022-12-12 19:53:41 +0530

    11_12_2022

--- 10_12_2022/01_basic.js
+++ 10_12_2022/01_basic.js
@@ -2,10 +2,36 @@ let a=3;
 let b=4;
 let x=true;
 let y=null;
-console.log(typeof(x));
-console.log(typeof(y));
+// console.log(typeof(x));
+// console.log(typeof(y));
 let c=[1,"praveer",true];
 
-console.log("two number is",a,b);
-console.log("two number is" +a+b);
+// console.log("two number is",a,b);
+// console.log("two number is" +a+b);
+
+
+let obj = {
+    fname:"praveer",
+    lname:"pratap"
+}
+
+// const a; error
+// const d=10;error
+// d=30;
+// console.log(d);
+
+console.log(`My fname is ${2+7}`);//Template literal
+
+
+if(10=="10"){
+    console.log("Used == comparision operator");
+}
+if(10==="10"){
+
+}else{
+    console.log("Used === comparision operator");
+}
+
+
+
 
---
+++ 11_12_2022/01_conditions.js
@@ -0,0 +1,36 @@
+let rating = 5;
+//if and else
+// if(rating==5){
+//     console.log("Rating is 5");
+// }else{
+//     console.log("rating is not 5");
+// }
+
+//switch case
+
+// let a=10;
+// switch(a){
+//     case 1:{
+//         console.log("Number is 1");
+//         break;
+//     }
+//     case "10":console.log("String version of 10");
+//     case 10:console.log("Number is 10");
+//     break;
+//     default:console.log("Different number");
+//     case 10:console.log("Number is 10 again");
+//     // default:console.log("2nd default statement");error
+//     break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
