5e5c7d9e35e6ade36c02acaba418a77bd2cb63c2
Praveerpratap2803 <praveerpratap2803@gmail.com>
2022-12-17 23:14:25 +0530

    Working on date 17_12_2022

---
+++ 17_12_2022/index.js
@@ -0,0 +1,23 @@
+let name1 = ['a','b','c'];
+
+// console.log(name1);
+// console.log(name1.length);
+// console.log(name1[2]);
+// console.log(name1[name1.length-1]);
+// name1[1]='x';
+
+let name2= new Array(1,2,3);
+console.log(typeof(name1));
+console.log(typeof(name2));
+
+// names1.push('d');
+
+
+
+// let names2 = ['a','b','c'];
+// for(let a of names2){
+//     if(a==='b'){
+//         console.log("error");
+//     }
+    
+// }
